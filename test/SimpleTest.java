5ffa4ddda22a9d21e3babc437767fd3b13beb675
qpham843 <32086045+qpham843@users.noreply.github.com>
2019-04-22 14:19:09 -0700

    Added a 2 = 2 test.

--- test/SimpleTest.java
+++ test/SimpleTest.java
@@ -7,4 +7,9 @@ public class SimpleTest {
     public void simpleTestReturns0() {
         assertEquals(1, 1);
     }
+
+    @Test
+    public void anotherSillyTest() {
+        assertEquals(2, 2);
+    }
 }
